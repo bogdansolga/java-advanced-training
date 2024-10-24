0529436ee7dacd09755f320ceb68b5bc836fe402
Bogdan Solga <bogdan.solga@gmail.com>
2024-10-24 16:48:12 +0300

    [dev] Added a few more CompletableFuture methods

--- d02/01-multi-threading-samples/src/main/java/net/safedata/java/advanced/training/completablefuture/CompletableFuturesExamples.java
+++ d02/01-multi-threading-samples/src/main/java/net/safedata/java/advanced/training/completablefuture/CompletableFuturesExamples.java
@@ -1,10 +1,42 @@
 package net.safedata.java.advanced.training.completablefuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class CompletableFuturesExamples {
 
     public static void main(String[] args) {
+        newExamples();
+
+        //oldExamples();
+    }
+
+    private static void newExamples() {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello, CF!");
+        completableFuture.thenApply(String::toUpperCase) // intermediary op, returns a CF<Type>
+                         .thenComposeAsync(value -> CompletableFuture.supplyAsync(() -> "World!"))
+                         .handleAsync((value, exception) -> { // BiFunction, returns the value type
+                             return exception.getMessage();
+                         })
+                         .whenCompleteAsync((value, exception) -> { //BiConsumer, does not return anything
+                             if (value != null) {
+                                 System.out.println("do something with the value");
+                             }
+                         })
+                         .completeOnTimeout("something", 1000, TimeUnit.MILLISECONDS)
+                         .exceptionallyAsync(Throwable::getMessage)
+                         .thenAccept(System.out::println)
+                         .thenRunAsync(() -> System.out.println("Done!"))
+        ;
+
+        final CompletableFuture<String> completableFuture1 = completableFuture.thenApplyAsync(value -> value.toUpperCase());
+        final CompletableFuture<String> completableFuture2 = completableFuture.thenComposeAsync(value -> CompletableFuture.supplyAsync(() -> "World!"));
+
+        completableFuture.thenAcceptAsync(value ->
+                System.out.println(Thread.currentThread().getName() + " - value: " + value)).join();
+    }
+
+    private static void oldExamples() {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Hello");
         //System.out.println(completableFuture.join());
 
